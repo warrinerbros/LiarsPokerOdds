a5f0c9e029608958aa37c6219389e523e727a838
Nick Warriner <nwarriner@magicwrighter.com>
2023-11-20 13:35:36 -0500

    Initial Commit

---
+++ LiarsPokerOdds/LiarsPokerOddsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiarsPokerOddsApp.swift
+//  LiarsPokerOdds
+//
+//  Created by Nick Warriner on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct LiarsPokerOddsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
